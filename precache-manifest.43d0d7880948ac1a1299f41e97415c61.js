a9c09a6d95582f551861be5048b895f7715b042c
veeresh <veerreshr@gmail.com>
2020-08-06 00:11:52 +0530

    Updates

---
+++ precache-manifest.43d0d7880948ac1a1299f41e97415c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88c0214e943391318bb74fccef6c0c6",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "7c1fd65d72c352b67a92",
+    "url": "/react-calculator/static/css/main.bd4dd6a1.chunk.css"
+  },
+  {
+    "revision": "d2af7708416aabaed2d3",
+    "url": "/react-calculator/static/js/2.84d55463.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.84d55463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1fd65d72c352b67a92",
+    "url": "/react-calculator/static/js/main.23a69aac.chunk.js"
+  },
+  {
+    "revision": "b5038e3bbe4dad79d7f3",
+    "url": "/react-calculator/static/js/runtime-main.4b719736.js"
+  }
+]);
